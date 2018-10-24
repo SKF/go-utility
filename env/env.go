d41190f74b367df1e87d23b9d79d06861421f459
stroem <jonathan.strom@gmail.com>
2018-10-24 14:08:36 +0200

    Update env parser (#14)

--- env/env.go
+++ env/env.go
@@ -1,28 +1,21 @@
-// Package env is a wrapper to os.Getenv
-
 package env
 
 import (
+	"log"
 	"os"
 	"strconv"
-
-	log "github.com/sirupsen/logrus"
 )
 
-// MustGetAsString returns value for given environment variable,
-// panics if not found
+// MustGetAsString returns value for given environment variable
 func MustGetAsString(variableName string) string {
 	value := os.Getenv(variableName)
 	if value == "" {
-		log.
-			WithField("variable", variableName).
-			Panic("System variable not set")
+		log.Panicf("System variable %s not set", variableName)
 	}
 	return value
 }
 
-// GetAsString returns value for given environment variable,
-// returns default if not found
+// GetAsString returns value for given environment variable, with default if not found
 func GetAsString(variableName string, defaultValue string) string {
 	value := os.Getenv(variableName)
 	if value == "" {
@@ -31,43 +24,41 @@ func GetAsString(variableName string, defaultValue string) string {
 	return value
 }
 
-// GetAsFloat returns float64 value for given environment variable,
-// returns default if not found or couldn't be parsed.
+// GetAsFloat returns value for given environment variable, with default if not found
 func GetAsFloat(variableName string, defaultValue float64) (floatValue float64) {
 	stringValue := os.Getenv(variableName)
 	if stringValue == "" {
 		return defaultValue
 	}
-
 	var err error
 	if floatValue, err = strconv.ParseFloat(stringValue, 64); err != nil {
-		log.
-			WithField("variable", variableName).
-			WithField("value", stringValue).
-			WithField("error", err).
-			Error("Failed to parse string to float")
-		return defaultValue
+		log.Panicf("Failed to parse string %s to float - %+v", stringValue, err)
 	}
 	return
 }
 
-// GetAsInt returns int value for given environment variable,
-// returns default if not found or couldn't be parsed.
+// GetAsInt returns value for given environment variable, with default if not found
 func GetAsInt(variableName string, defaultValue int) (intValue int) {
 	stringValue := os.Getenv(variableName)
 	if stringValue == "" {
 		return defaultValue
 	}
-
 	var err error
 	if intValue, err = strconv.Atoi(stringValue); err != nil {
-		log.
-			WithField("variable", variableName).
-			WithField("value", stringValue).
-			WithField("error", err).
-			Error("Failed to parse string to int")
+		log.Panicf("Failed to parse string %s to int - %+v", stringValue, err)
+	}
+	return
+}
+
+// GetAsBool returns value for given environment variable, with default if not found
+func GetAsBool(variableName string, defaultValue bool) (boolValue bool) {
+	stringValue := os.Getenv(variableName)
+	if stringValue == "" {
 		return defaultValue
 	}
-
+	var err error
+	if boolValue, err = strconv.ParseBool(stringValue); err != nil {
+		log.Panicf("Failed to parse string %s to bool - %+v", stringValue, err)
+	}
 	return
 }
