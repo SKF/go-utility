7bef921d20978320fd11009a59ec7b29df85bcf1
Fredrik Lönnblad <f.lonnblad@gmail.com>
2019-07-19 16:25:35 +0200

    made a fix

--- log/logger.go
+++ log/logger.go
@@ -34,15 +34,6 @@ func (l logger) WithTracing(ctx context.Context) (returnedLogger Logger) {
 		returnedLogger = l.
 			WithField("trace_id", binary.BigEndian.Uint64(traceID[8:])).
 			WithField("span_id", binary.BigEndian.Uint64(spanID[:]))
-
-		// returnedLogger = l.
-		// 	WithField("dd", struct {
-		// 		TraceID uint64 `json:"trace_id"`
-		// 		SpanID  uint64 `json:"span_id"`
-		// 	}{
-		// 		TraceID: binary.BigEndian.Uint64(traceID[8:]),
-		// 		SpanID:  binary.BigEndian.Uint64(spanID[:]),
-		// 	})
 	}
 	return
 }
