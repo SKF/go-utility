777e2939ab5cac673e70ef58e636c9dd634d7685
Fredrik Lönnblad <fredrik+github@lonnblad.com>
2019-08-28 13:27:49 +0200

    Added charset=utf-8 as a valid content-type parameter (#38)

--- http-middleware/cors.go
+++ http-middleware/cors.go
@@ -3,10 +3,6 @@ package httpmiddleware
 import (
 	"net/http"
 	"strings"
-
-	http_model "github.com/SKF/go-utility/http-model"
-	http_server "github.com/SKF/go-utility/http-server"
-	"github.com/SKF/go-utility/log"
 )
 
 // CorsMiddleware adds CORS headers to requests.
@@ -43,26 +39,5 @@ func Options(methods, headers []string) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Methods", methodsJoined)
 		w.Header().Set("Access-Control-Allow-Headers", headersJoined)
-	}
-}
-
-// ContentType wraps a HandlerFunc and checks the incoming
-// content-type with a list of allowed content types.
-func ContentType(next http.HandlerFunc, contentTypes ...string) http.HandlerFunc {
-	var validContentTypes = make(map[string]bool)
-	for _, contentType := range contentTypes {
-		validContentTypes[contentType] = true
-	}
-
-	return func(w http.ResponseWriter, req *http.Request) {
-		reqContentType := req.Header.Get("Content-Type")
-		if validContentTypes[reqContentType] {
-			next(w, req)
-			return
-		}
-
-		ctx := req.Context()
-		log.WithTracing(ctx).WithField("contentType", reqContentType).Warn("Unsupported Content-Type")
-		http_server.WriteJSONResponse(ctx, w, http.StatusUnsupportedMediaType, http_model.ErrResponseUnsupportedMediaType)
 	}
 }
