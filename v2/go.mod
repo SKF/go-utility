f6a383f2fbc50982c238260d2bb474e5e4d684db
Erik Karlsson <erik.karlsson@knowit.se>
2020-02-21 13:29:37 +0100

    Added packge for sqs and a message to put an object on a sqs queue

--- v2/go.mod
+++ v2/go.mod
@@ -3,22 +3,16 @@ module github.com/SKF/go-utility/v2
 go 1.13
 
 require (
-	github.com/DataDog/datadog-go v2.2.0+incompatible // indirect
 	github.com/DataDog/opencensus-go-exporter-datadog v0.0.0-20190726131236-c7b72e836616
+	github.com/SKF/go-utility v1.10.2
 	github.com/SKF/proto v1.21.0-go
 	github.com/aws/aws-lambda-go v1.12.0
 	github.com/aws/aws-sdk-go v1.25.49
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
-	github.com/golang/protobuf v1.3.2 // indirect
-	github.com/google/uuid v0.0.0-20161128191214-064e2069ce9c
+	github.com/google/uuid v1.0.0
 	github.com/gorilla/mux v1.7.3
-	github.com/hashicorp/golang-lru v0.5.3 // indirect
-	github.com/philhofer/fwd v1.0.0 // indirect
 	github.com/pkg/errors v0.8.1
 	github.com/stretchr/testify v1.4.0
-	github.com/tinylib/msgp v1.1.0 // indirect
 	go.opencensus.io v0.22.0
 	go.uber.org/zap v1.12.0
-	golang.org/x/net v0.0.0-20190724013045-ca1201d0de80 // indirect
-	gopkg.in/DataDog/dd-trace-go.v1 v1.16.1 // indirect
 )
---
+++ v2/sqs/sendtoqueue.go
@@ -0,0 +1,70 @@
+package sqs
+
+import (
+	"context"
+	"encoding/json"
+	"errors"
+
+	"github.com/SKF/go-utility/log"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/sqs"
+)
+
+var queueURL string
+var sqsClient *sqs.SQS
+
+const errQueueURLNotConfigured = "queue URL not configured"
+const errQueueClientNotConfigured = "queue client not configured"
+
+func ConfigureQueue(ctx context.Context, queue string, svc *sqs.SQS) {
+	sqsClient = svc
+	queueURL = queue
+}
+
+func PutObjectOnQueue(ctx context.Context, object interface{}) error {
+	if queueURL == "" {
+		err := errors.New(errQueueURLNotConfigured)
+		log.Warn(errQueueURLNotConfigured)
+
+		return err
+	}
+
+	if sqsClient == nil {
+		err := errors.New(errQueueClientNotConfigured)
+		log.Warn(errQueueClientNotConfigured)
+
+		return err
+	}
+
+	jsonStr, marshalErr := requestToJSON(object)
+	if marshalErr != nil {
+		return marshalErr
+	}
+
+	input := sqs.SendMessageInput{
+		MessageBody: aws.String(jsonStr),
+		QueueUrl:    &queueURL,
+	}
+	_, err := sqsClient.SendMessageWithContext(ctx, &input)
+
+	if err != nil {
+		log.WithError(err).
+			Error("Unable to Send message to queue")
+		return err
+	}
+
+	return nil
+}
+
+func requestToJSON(object interface{}) (string, error) {
+	bodyByteARR, err := json.Marshal(object)
+	if err != nil {
+		log.
+			WithError(err).
+			Error("Unable to marshal object to put on queue to json")
+
+		return "", err
+	}
+
+	return string(bodyByteARR), nil
+}
