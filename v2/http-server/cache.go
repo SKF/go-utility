90b445d9867e8c5114abd23bcac7ee38df058775
Fredrik Lönnblad <fredrik+github@lonnblad.com>
2020-02-24 09:42:26 -0300

    Ristretto didn't support structs as keys

--- v2/http-server/cache.go
+++ v2/http-server/cache.go
@@ -8,36 +8,16 @@ import (
 	"github.com/SKF/go-utility/v2/accesstokensubcontext"
 )
 
-type GeneralCacheKey struct {
-	Method   string
-	RawPath  string
-	RawQuery string
+func NewGeneralCacheKey(req *http.Request) string {
+	return req.Method + " :: " + req.URL.RawPath + " :: " + req.URL.RawQuery
 }
 
-type UserSpecificCacheKey struct {
-	GeneralCacheKey
-	AccessTokenSubject string
-}
-
-func NewGeneralCacheKey(req *http.Request) GeneralCacheKey {
-	return GeneralCacheKey{
-		Method:   req.Method,
-		RawPath:  req.URL.RawPath,
-		RawQuery: req.URL.RawQuery,
-	}
-}
-
-func NewUserSpecificCacheKey(ctx context.Context, req *http.Request) (_ UserSpecificCacheKey, err error) {
+func NewUserSpecificCacheKey(ctx context.Context, req *http.Request) (_ string, err error) {
 	subject, found := accesstokensubcontext.FromContext(ctx)
 	if !found {
 		err = errors.New("failed to extract Access Token Subject from context")
 		return
 	}
 
-	key := UserSpecificCacheKey{
-		GeneralCacheKey:    NewGeneralCacheKey(req),
-		AccessTokenSubject: subject,
-	}
-
-	return key, nil
+	return subject + " :: " + NewGeneralCacheKey(req), nil
 }
