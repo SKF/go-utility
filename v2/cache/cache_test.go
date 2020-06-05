b82bceae1510911e995f9f9ea48140f755bede68
stroem <jonathan@devies.se>
2020-06-05 22:45:24 +0200

    goimports

--- v2/cache/cache_test.go
+++ v2/cache/cache_test.go
@@ -5,10 +5,11 @@ import (
 	"testing"
 	"time"
 
-	"github.com/SKF/go-utility/v2/log"
 	"github.com/SKF/proto/v2/common"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+
+	"github.com/SKF/go-utility/v2/log"
 )
 
 const (
