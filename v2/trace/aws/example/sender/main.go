a995f80dd7bf814f2a16926ff95148cf9bbc8c95
stroem <jonathan.strom@gmail.com>
2020-11-27 15:21:47 +0100

    AWS wrapper for tracing (#99)
    * AWS tracing wrapper for SNS and SQS
    
    Co-authored-by: stroem <jonathan@devies.se>

---
+++ v2/trace/aws/example/sender/main.go
@@ -0,0 +1,44 @@
+// Example using AWS Wrapper to send trace headers
+
+package main
+
+import (
+	"context"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/client"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/sqs"
+	oc_trace "go.opencensus.io/trace"
+
+	"github.com/SKF/go-utility/v2/log"
+	aws_skf_trace "github.com/SKF/go-utility/v2/trace/aws"
+)
+
+func sendMessage(ctx context.Context, sess client.ConfigProvider, msg string) error {
+	// Context need to containing Datadog or B3 trace for this to work
+	ctx, span := oc_trace.StartSpan(ctx, "sendMessage")
+	defer span.End()
+
+	// Sending message and injecting tracing headers
+	client := sqs.New(sess)
+	_, err := client.SendMessageWithContext(ctx, &sqs.SendMessageInput{
+		MessageBody: aws.String(msg),
+	})
+
+	return err
+}
+
+func main() {
+	sess := session.Must(session.NewSession())
+
+	// Pass trace headers by wrapping the session with WrapSession
+	sess = aws_skf_trace.WrapSession(sess)
+
+	ctx := context.Background()
+	if err := sendMessage(ctx, sess, "Hello SKF!"); err != nil {
+		log.WithTracing(ctx).
+			WithError(err).
+			Error("Error sending message")
+	}
+}
