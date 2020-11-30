50d3bbf1d5b39353b7bdad62c7fff50b6f5261af
Anton Ekberg <anton.ekberg@skf.com>
2020-11-30 11:57:38 +0100

    Make cancelled authorizer calls not return 500

--- v2/http-middleware/security.go
+++ v2/http-middleware/security.go
@@ -14,6 +14,8 @@ import (
 	"github.com/SKF/go-utility/v2/jwt"
 	"github.com/SKF/go-utility/v2/log"
 	"github.com/SKF/go-utility/v2/useridcontext"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 
 	"github.com/SKF/proto/v2/common"
 	"github.com/gorilla/mux"
@@ -286,11 +288,18 @@ func checkAuthorization(ctx context.Context, req *http.Request, authorizer Autho
 			resource,
 		)
 		if err != nil {
-			logFields.WithError(err).
+			logFields = logFields.WithError(err).
 				WithField("userId", userID).
 				WithField("action", authorizeConfig.action).
-				WithField("resource", resource).
-				Error("Error when calling IsAuthorized")
+				WithField("resource", resource)
+
+			if grpcStatus := status.Code(err); grpcStatus == codes.Canceled {
+				logFields.Debug("Cancelled context when calling IsAuthorized")
+
+				return false, nil
+			}
+
+			logFields.Error("Error when calling IsAuthorized")
 
 			return false, err
 		}
