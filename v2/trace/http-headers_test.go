ec36b62c477c59454495da5ffa41ab29fbcf55d6
Fredrik Lönnblad <fredrik+github@lonnblad.com>
2020-12-04 14:20:29 +0100

    Added testing of trace http headers

---
+++ v2/trace/http-headers_test.go
@@ -0,0 +1,24 @@
+package trace_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/SKF/go-utility/v2/array"
+	"github.com/SKF/go-utility/v2/trace"
+)
+
+func Test_AllB3Headers(t *testing.T) {
+	const expectedLength = 3
+
+	actual := array.DistinctString(trace.AllB3Headers())
+	assert.Len(t, actual, expectedLength)
+}
+
+func Test_AllDatadogHeaders(t *testing.T) {
+	const expectedLength = 5
+
+	actual := array.DistinctString(trace.AllDatadogHeaders())
+	assert.Len(t, actual, expectedLength)
+}
