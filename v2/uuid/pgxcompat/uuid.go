6ffa50d49362d413efefb0fffb051c2f87fb88b3
Johan Sandelin <johan@sandelin.nu>
2021-09-08 16:07:12 +0200

    More lint

--- v2/uuid/pgxcompat/uuid.go
+++ v2/uuid/pgxcompat/uuid.go
@@ -28,44 +28,44 @@ type Getter interface {
 	Get() interface{}
 }
 
-func (dst *UUID) Set(src interface{}) error { // nolint:gocyclo
-	if src == nil {
-		*dst = UUID{Status: pgtype.Null}
+func (u *UUID) Set(value interface{}) error { // nolint:gocyclo
+	if value == nil {
+		*u = UUID{Status: pgtype.Null}
 		return nil
 	}
 
-	if value, ok := src.(Getter); ok {
-		value2 := value.Get()
-		if value2 != value {
-			return dst.Set(value2)
+	if getterValue, ok := value.(Getter); ok {
+		value2 := getterValue.Get()
+		if value2 != getterValue {
+			return u.Set(value2)
 		}
 	}
 
-	switch value := src.(type) {
+	switch value := value.(type) {
 	case uuid.UUID:
-		*dst = UUID{UUID: value, Status: pgtype.Present}
+		*u = UUID{UUID: value, Status: pgtype.Present}
 	case [16]byte:
 		uuidStr, _ := fromBinary(value[:]) // nolint:errcheck // we know input is 16 bytes
-		*dst = UUID{UUID: uuid.UUID(uuidStr), Status: pgtype.Present}
+		*u = UUID{UUID: uuid.UUID(uuidStr), Status: pgtype.Present}
 	case []byte:
 		if value == nil {
-			*dst = UUID{Status: pgtype.Null}
+			*u = UUID{Status: pgtype.Null}
 		} else if uuidStr, err := fromBinary(value[:]); err != nil {
 			return err
 		} else {
-			*dst = UUID{UUID: uuid.UUID(uuidStr), Status: pgtype.Present}
+			*u = UUID{UUID: uuid.UUID(uuidStr), Status: pgtype.Present}
 		}
 	case string:
 		if len(value) == uuidStrShortLength {
 			value = fmt.Sprintf("%s-%s-%s-%s-%s", value[0:8], value[8:12], value[12:16], value[16:20], value[20:32])
 		}
 
-		*dst = UUID{UUID: uuid.UUID(value), Status: pgtype.Present}
+		*u = UUID{UUID: uuid.UUID(value), Status: pgtype.Present}
 	case *string:
 		if value == nil {
-			*dst = UUID{Status: pgtype.Null}
+			*u = UUID{Status: pgtype.Null}
 		} else {
-			return dst.Set(*value)
+			return u.Set(*value)
 		}
 	default:
 		return fmt.Errorf("cannot convert %v of type %T to UUID", value, value)
@@ -74,135 +74,135 @@ func (dst *UUID) Set(src interface{}) error { // nolint:gocyclo
 	return nil
 }
 
-func (dst UUID) Get() interface{} {
-	switch dst.Status {
+func (u UUID) Get() interface{} {
+	switch u.Status {
 	case pgtype.Present:
-		return dst.UUID
+		return u.UUID
 	case pgtype.Null:
 		return nil
 	default:
-		return dst.Status
+		return u.Status
 	}
 }
 
-func (src *UUID) AssignTo(dst interface{}) (err error) {
-	switch src.Status {
+func (u *UUID) AssignTo(target interface{}) (err error) {
+	switch u.Status {
 	case pgtype.Present:
-		switch v := dst.(type) {
+		switch dst := target.(type) {
 		case *uuid.UUID:
-			*v = src.UUID
+			*dst = u.UUID
 			return nil
 		case *[16]byte:
-			*v, err = toBinary(src.UUID.String())
+			*dst, err = toBinary(u.UUID.String())
 			return err
 		case *[]byte:
-			*v = make([]byte, uuidBinaryLength)
-			bin, err := toBinary(src.UUID.String())
-			copy(*v, bin[:])
+			*dst = make([]byte, uuidBinaryLength)
+			bin, err := toBinary(u.UUID.String())
+			copy(*dst, bin[:])
 
 			return err
 		case *string:
-			*v = src.UUID.String()
+			*dst = u.UUID.String()
 			return nil
 		default:
-			if nextDst, retry := pgtype.GetAssignToDstType(v); retry {
-				return src.AssignTo(nextDst)
+			if nextDst, retry := pgtype.GetAssignToDstType(dst); retry {
+				return u.AssignTo(nextDst)
 			}
 
 			return fmt.Errorf("unable to assign to %T", dst)
 		}
 	case pgtype.Null:
-		return pgtype.NullAssignTo(dst)
+		return pgtype.NullAssignTo(target)
 	}
 
-	return fmt.Errorf("cannot assign %v into %T", src, dst)
+	return fmt.Errorf("cannot assign %v into %T", u, target)
 }
 
-func (dst *UUID) DecodeText(ci *pgtype.ConnInfo, src []byte) error {
-	if src == nil {
-		*dst = UUID{Status: pgtype.Null}
+func (u *UUID) DecodeText(ci *pgtype.ConnInfo, value []byte) error {
+	if value == nil {
+		*u = UUID{Status: pgtype.Null}
 		return nil
 	}
 
-	u := uuid.UUID(string(src))
-	if err := u.Validate(); err != nil {
+	uuidValue := uuid.UUID(string(value))
+	if err := uuidValue.Validate(); err != nil {
 		return err
 	}
 
-	*dst = UUID{UUID: u, Status: pgtype.Present}
+	*u = UUID{UUID: uuidValue, Status: pgtype.Present}
 
 	return nil
 }
 
-func (dst *UUID) DecodeBinary(ci *pgtype.ConnInfo, src []byte) error {
-	if src == nil {
-		*dst = UUID{Status: pgtype.Null}
+func (u *UUID) DecodeBinary(ci *pgtype.ConnInfo, value []byte) error {
+	if value == nil {
+		*u = UUID{Status: pgtype.Null}
 		return nil
 	}
 
-	uuidStr, err := fromBinary(src)
+	uuidValue, err := fromBinary(value)
 	if err != nil {
 		return err
 	}
 
-	*dst = UUID{UUID: uuid.UUID(uuidStr), Status: pgtype.Present}
+	*u = UUID{UUID: uuid.UUID(uuidValue), Status: pgtype.Present}
 
 	return nil
 }
 
-func (src UUID) EncodeText(ci *pgtype.ConnInfo, buf []byte) ([]byte, error) {
-	switch src.Status {
+func (u UUID) EncodeText(ci *pgtype.ConnInfo, outBuf []byte) ([]byte, error) {
+	switch u.Status {
 	case pgtype.Null:
 		return nil, nil
 	case pgtype.Undefined:
 		return nil, errUndefined
 	}
 
-	return append(buf, src.UUID.String()...), nil
+	return append(outBuf, u.UUID.String()...), nil
 }
 
-func (src UUID) EncodeBinary(ci *pgtype.ConnInfo, buf []byte) ([]byte, error) {
-	switch src.Status {
+func (u UUID) EncodeBinary(ci *pgtype.ConnInfo, outBuf []byte) ([]byte, error) {
+	switch u.Status {
 	case pgtype.Null:
 		return nil, nil
 	case pgtype.Undefined:
 		return nil, errUndefined
 	}
 
-	tmpBuf, err := toBinary(src.UUID.String())
+	tmpBuf, err := toBinary(u.UUID.String())
 	if err != nil {
 		return nil, err
 	}
 
-	return append(buf, tmpBuf[:]...), nil
+	return append(outBuf, tmpBuf[:]...), nil
 }
 
 // Scan implements the database/sql Scanner interface.
-func (dst *UUID) Scan(src interface{}) error {
-	if src == nil {
-		*dst = UUID{Status: pgtype.Null}
+func (u *UUID) Scan(value interface{}) error {
+	if value == nil {
+		*u = UUID{Status: pgtype.Null}
 		return nil
 	}
 
-	switch src := src.(type) {
+	switch value := value.(type) {
 	case string:
-		return dst.DecodeText(nil, []byte(src))
+		return u.DecodeText(nil, []byte(value))
 	case []byte:
-		return dst.DecodeText(nil, src)
+		return u.DecodeText(nil, value)
 	}
 
-	return fmt.Errorf("cannot scan %T", src)
+	return fmt.Errorf("cannot scan %T", value)
 }
 
 // Value implements the database/sql/driver Valuer interface.
-func (src UUID) Value() (driver.Value, error) {
-	return pgtype.EncodeValueText(src)
+func (u UUID) Value() (driver.Value, error) {
+	return pgtype.EncodeValueText(u)
 }
 
-func (src UUID) MarshalJSON() ([]byte, error) {
-	switch src.Status {
+func (u UUID) MarshalJSON() ([]byte, error) {
+	switch u.Status {
 	case pgtype.Present:
-		return []byte(`"` + src.UUID.String() + `"`), nil
+		return []byte(`"` + u.UUID.String() + `"`), nil
 	case pgtype.Null:
 		return []byte("null"), nil
 	case pgtype.Undefined:
@@ -212,16 +212,16 @@ func (src UUID) MarshalJSON() ([]byte, error) {
 	return nil, errBadStatus
 }
 
-func (dst *UUID) UnmarshalJSON(b []byte) error {
-	*dst = UUID{UUID: uuid.EmptyUUID, Status: pgtype.Undefined}
+func (u *UUID) UnmarshalJSON(b []byte) error {
+	*u = UUID{UUID: uuid.EmptyUUID, Status: pgtype.Undefined}
 
-	if err := json.Unmarshal(b, &dst.UUID); err != nil {
+	if err := json.Unmarshal(b, &u.UUID); err != nil {
 		return err
 	}
 
-	dst.Status = pgtype.Null
-	if dst.UUID.IsValid() && dst.UUID != uuid.EmptyUUID {
-		dst.Status = pgtype.Present
+	u.Status = pgtype.Null
+	if u.UUID.IsValid() && u.UUID != uuid.EmptyUUID {
+		u.Status = pgtype.Present
 	}
 
 	return nil
